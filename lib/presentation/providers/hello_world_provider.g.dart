8a99990906b2bcd227f824d0b32cef27a3daf830
Roberto Arrioja <r@robertoarrioja.dev>
2023-08-25 18:42:27 -0500

    01_provider listo

---
+++ lib/presentation/providers/hello_world_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hello_world_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloWorldHash() => r'1db99e9343a05546226f0390a58718b9c3133122';
+
+/// See also [helloWorld].
+@ProviderFor(helloWorld)
+final helloWorldProvider = AutoDisposeProvider<String>.internal(
+  helloWorld,
+  name: r'helloWorldProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloWorldHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloWorldRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
