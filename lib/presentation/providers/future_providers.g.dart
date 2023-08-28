2d8ddefbe14fbe6df3e5f59f5de58674dd26a1ef
Roberto Arrioja <r@robertoarrioja.dev>
2023-08-28 18:38:35 -0500

    05 Future Provider

---
+++ lib/presentation/providers/future_providers.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'future_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pokemonNameHash() => r'e52072fbe4decee38dbc9b93478532a5f94c7e86';
+
+/// See also [pokemonName].
+@ProviderFor(pokemonName)
+final pokemonNameProvider = FutureProvider<String>.internal(
+  pokemonName,
+  name: r'pokemonNameProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pokemonNameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PokemonNameRef = FutureProviderRef<String>;
+String _$pokemonIdHash() => r'73bbaaca0ccbe42099d794f151f9a52fbcc311e3';
+
+/// See also [PokemonId].
+@ProviderFor(PokemonId)
+final pokemonIdProvider = NotifierProvider<PokemonId, int>.internal(
+  PokemonId.new,
+  name: r'pokemonIdProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pokemonIdHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PokemonId = Notifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
--- lib/presentation/screens/05_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/05_future_provider/future_provider_screen.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class FutureProviderScreen extends StatelessWidget {
+class FutureProviderScreen extends ConsumerWidget {
   const FutureProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonName = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonName.when(
+          data: (data) => Text(data),
+          loading: () => const CircularProgressIndicator(),
+          error: (error, stackTrace) => Text('$error'),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'btn-1',
+            child: const Icon(Icons.refresh),
+            onPressed: () {
+              //ref.invalidate(pokemonNameProvider);
+              ref.read(pokemonIdProvider.notifier).nextPokemon();
+            },
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            child: const Icon(Icons.minimize_outlined),
+            onPressed: () {
+              //ref.invalidate(pokemonNameProvider);
+              ref.read(pokemonIdProvider.notifier).prevPokemon();
+            },
+          ),
+        ],
       ),
     );
   }
